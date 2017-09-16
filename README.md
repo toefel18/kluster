3ee32254cbe3273711740f6811d672e11d055d38
Christophe <toefel18@gmail.com>
2017-09-16 17:02:39 +0200

    Initial commit: simple postgres/kafka adapter.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # kluster
 cluster of N postgres instances using Kafka as an in-sync mechanism
+
+
+Spin up kafka and postgres
+```sh
+docker run --name kluster-kafka -d -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=localhost --env ADVERTISED_PORT=9092 --env TOPICS=kluster-mutation,kluster-response spotify/kafka
+            
+docker run --name kluster-postgres-1 -d -p 20000:5432 -e POSTGRES_USER=kluster -e POSTGRES_PASSWORD=kluster -d postgres
+docker run --name kluster-postgres-2 -d -p 20001:5432 -e POSTGRES_USER=kluster -e POSTGRES_PASSWORD=kluster -d postgres
+
+
+```
---
+++ kafka-pg-adapter/kafka-postgres-adapter.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"log"
+	"os"
+	"github.com/bsm/sarama-cluster"
+	"github.com/satori/go.uuid"
+	"os/signal"
+	"database/sql"
+	_ "github.com/lib/pq"
+	"github.com/Shopify/sarama"
+)
+
+func main() {
+	log.Println("Starting Kafka -> Postgres adapter")
+	pgConnectionString := mustHaveEnvironment("DATABASE_ADDRESS")
+	kafkaBootstrapServers := mustHaveEnvironment("KAFKA_BOOTSTRAP_SERVERS")
+	kafkaMutationTopic := mustHaveEnvironment("KAFKA_MUTATION_TOPIC")
+	kafkaResponseTopic := mustHaveEnvironment("KAFKA_RESPONSE_TOPIC")
+
+	db, err := sql.Open("postgres", pgConnectionString)
+	if err != nil {
+		log.Fatalf("Error connecting to postgres: %v", err.Error())
+	}
+	log.Printf("Connected to DB %v", pgConnectionString)
+
+	if err := consumeMutations(kafkaBootstrapServers, kafkaMutationTopic, kafkaResponseTopic, db); err != nil {
+		log.Fatalf("Error with Kafka %v", err.Error())
+	}
+
+}
+
+func consumeMutations(bootstrapServers string, mutationTopic string, responseTopic string, db *sql.DB) error {
+	config := cluster.NewConfig()
+	config.Consumer.Return.Errors = true
+	config.Group.Return.Notifications = true
+
+	brokers := []string{bootstrapServers}
+	mutationConsumer, err := cluster.NewConsumer(brokers, uuid.NewV4().String(), []string{mutationTopic}, config)
+	if err != nil {
+		return err
+	}
+	defer mutationConsumer.Close()
+
+	programInterrupted := make(chan os.Signal, 1)
+	signal.Notify(programInterrupted, os.Interrupt)
+
+	log.Println("Listening for kafka messages")
+
+	for {
+		select {
+		case msg, ok := <-mutationConsumer.Messages():
+			if ok {
+				log.Printf("Received kafka message, key=%v val=%v", string(msg.Key), string(msg.Value))
+				executeDatabaseMsg(msg, db)
+			}
+		case msg, ok := <-mutationConsumer.Notifications():
+			if ok {
+				log.Printf("Received kafka balancing notification claimed=%v, released=%v, current=%v", msg.Claimed, msg.Released, msg.Current)
+			}
+		case err, ok := <-mutationConsumer.Errors():
+			if ok {
+				log.Printf("Received kafka error %v", err.Error())
+			}
+		case <-programInterrupted:
+			return nil
+		}
+	}
+}
+func executeDatabaseMsg(message *sarama.ConsumerMessage, db *sql.DB) {
+	query := string(message.Value)
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Printf("Error while executing query %v, error: %v", query, err.Error())
+	}
+	// TODO return result on response topic
+	log.Printf("Successfully executed %v, result ", query)
+}
+
+func mustHaveEnvironment(name string) string {
+	val, set := os.LookupEnv(name)
+	if !set {
+		log.Fatalf("could not find required environment variable %v", name)
+	}
+	return val
+}
